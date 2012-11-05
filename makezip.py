9766331f4f738eed48c1f934ac2e00ddecd2d36b
Jeff Trawick <trawick@gmail.com>
2012-11-05 12:40:28 +0100

    initial versions

---
+++ makezip.py
@@ -0,0 +1,47 @@
+import os
+import string
+import subprocess
+import sys
+
+source_files = ['*.c', '*.h']
+meta_files = ['CHANGES.txt', 'LICENSE', 'NOTICE', 'README']
+bin_files = []
+
+version = '0.1'
+
+dirname = 'mod_hook_ar-%s' % (version)
+
+if os.path.exists(dirname):
+    raise Exception("%s should not already exist" % (dirname))
+
+if os.path.exists(dirname + '.zip'):
+    raise Exception("zip should not already exist")
+
+os.mkdir(dirname, 0755)
+
+cmd = ['cp', '-pR'] + source_files + meta_files + bin_files + [dirname]
+
+print cmd
+
+cmd = string.join(cmd)
+print cmd
+
+try:
+    rc = subprocess.call(cmd, shell=True)
+except:
+    print "couldn't run, error", sys.exc_info()[0]
+    raise
+
+if rc != 0:
+    print "rc:", rc
+    sys.exit(1)
+
+try:
+    rc = subprocess.call(['zip', '-r', '%s.zip' % dirname, dirname])
+except:
+    print "couldn't run, error", sys.exc_info()[0]
+    raise
+
+if rc != 0:
+    print "rc:", rc
+    sys.exit(1)
